d2698a1302c6b5b806aa63443f7d07c268368f3c
Celso Delfino <celsinhodelfino@hotmail.com>
2023-07-10 18:13:12 -0300

    4verbos

---
+++ RestWithAspNet/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithAspNet.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+    }
+}
