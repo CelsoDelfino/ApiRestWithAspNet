bb781a942421649d0d00f0a4ab140ef408e56364
Celso Delfino <celsinhodelfino@hotmail.com>
2023-07-11 17:35:20 -0300

    4verbos finish

--- RestWithAspNet/Program.cs
+++ RestWithAspNet/Program.cs
@@ -1,8 +1,13 @@
+using RestWithAspNet.Services;
+using RestWithAspNet.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ RestWithAspNet/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,65 @@
+﻿using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName= "Celso",
+                LastName = "Delfino",
+                Adress = "Rio de Janeiro",
+                Gender = "Masculino"
+            };
+        }
+
+        public List<Person> GetAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i =0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person name" + i,
+                LastName = "Person Last name" + i,
+                Adress = "Some Adress" + i ,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
